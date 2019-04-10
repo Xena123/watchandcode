44454e79182f5db69a6fa048f8864b6950dc622b
Cheryl Casteling <cheryl.casteling@unlimtedgroup.com>
2019-04-10 10:12:52 +0100

    added some comments to version 7

--- todo_list_v7.js
+++ todo_list_v7.js
@@ -1,13 +1,17 @@
 todoList = {
   todos: [],
   displayTodos: function() {
+    // if there are no todos then print the following:
     if (this.todos.length == 0) {
       console.log("Your todo list is empty")
+    // if there are any todos then iterate through the todos
     } else {
       console.log('My Todos:');
       for (var i = 0; i < this.todos.length; i++) {
+        // check if the the todo is completed and print (x) for completed
         if (this.todos[i].completed == true) {
           console.log("(x)", this.todos[i].todoText);
+        // otherwise print and ( ) for incomplete
         } else {
           console.log("( )", this.todos[i].todoText);
         }
@@ -32,21 +36,25 @@ todoList = {
   
   toggleCompleted: function(index) {
     var todo = this.todos[index];
+    // make the boolean on completed the opposite to what it is now
     todo.completed = !todo.completed;
     this.displayTodos();
   },
   toggleAll: function() {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
+    // iterate through the array and count all the completed todos
     for (var i = 0; i < totalTodos; i++) {
       if (this.todos[i].completed == true) {
         completedTodos++;
       }
     };
+    // if all the todos are completed (true) then change to incomplete (false)
     if (completedTodos == totalTodos) {
       for (var i = 0; i < totalTodos; i++) {
         this.todos[i].completed = false;
       };
+    // otherwise change all to completed (true)
     } else {
       for (var i = 0; i < totalTodos; i++) {
         this.todos[i].completed = true;
@@ -55,6 +63,8 @@ todoList = {
     this.displayTodos();
   }
 };
+
+// Version 7 additions
 
 // Clicking "Display todos" should run todoList.displayTodos
 var displayTodosButton = document.getElementById("btn--display");
